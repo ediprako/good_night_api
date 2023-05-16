a0c5c60b09f1a4f0d72d4bffe416bdfe5c6f5cdf
edi prakoso <ediprakoso@gmail.com>
2023-05-17 06:22:12 +0700

    feat: add user migration and model with generate token mechanism

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  def persist_with_random_token!(attempts = 10)
+    retries ||= 0
+    self.access_token = SecureRandom.urlsafe_base64(nil, false)
+    save!
+  rescue ActiveRecord::RecordNotUnique
+    raise if (retries += 1) > attempts
+
+    Rails.logger.warn("random token, unlikely collision number #{retries}")
+    self.access_token = SecureRandom.urlsafe_base64(16, false)
+    retry
+  end
+end
