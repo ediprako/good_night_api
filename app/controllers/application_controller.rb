3e69f9592afbaa1ceac6790d226cc6916d35dee2
edi prakoso <ediprakoso@gmail.com>
2023-05-17 06:23:14 +0700

    feat: add support method for rendering response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def render_error(message, status = :internal_server_error)
+    error = {
+      message: message,
+      status: status
+    }
+
+    render json: { error: error }, status: status
+  end
+
+  def render_success(data, status)
+    resp = {
+      data: data,
+      status: status
+    }
+
+    render json: resp, status: status
+  end
 end
